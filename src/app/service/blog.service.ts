4a511dfa184d5d7bbe05a421ecc3f403a12d3d41
1noob <448475691@qq.com>
2021-09-25 20:07:47 +0800

    new version

--- src/app/service/blog.service.ts
+++ src/app/service/blog.service.ts
@@ -3,13 +3,13 @@ import { ajax } from 'rxjs/ajax';
 import { of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { formatDate } from "@angular/common";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 // 内存池
 const cache = {};
 const headers = {
-  // 'Authorization': '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'
+  'Authorization': 'Basic dG9rZW46Z2hwX2dHalQ5aFRQUFlEVGxETkJYdkQ1aExuaDROOEJpajFYQWdtUQ=='
 };
 
 
@@ -50,7 +50,6 @@ export class BlogService {
 
   getBlog$(owner, repo, blogName) {
     const url = `https://raw.githubusercontent.com/${owner}/${repo}/main/${blogName}`;
-    // const url = `http://localhost:4300/assets/test.html`;
     return this.http.get(url).pipe(
       this.catchErrorPipe,
       map((data: any) => {
@@ -58,36 +57,4 @@ export class BlogService {
       })
     );
   }
-
-
-  // processBlog(blog) {
-  //   if (!blog.markdownRendered) {
-  //     this.renderMarkdown$(blog.body)
-  //       .subscribe(data => {
-  //         blog.body = data;
-  //         blog.markdownRendered = true;
-  //       });
-  //   }
-  // }
-  //
-  //
-  // renderMarkdown$(text) {
-  //   return ajax({
-  //     url: "https://api.github.com/markdown",
-  //     method: 'POST',
-  //     responseType: 'text',
-  //     body: {
-  //       text,
-  //       mode: 'gfm'
-  //     },
-  //     headers: {
-  //       'Accept': 'text/html',
-  //       'Content-Type': 'application/json;charset=UTF-8',
-  //       'Authorization': 'Basic dG9rZW46Z2hwX3dCaWtKYjVqclhiUkVTUlJ6S0kydjFXRkF6UVJLMTNQVnNvbw=='
-  //     }
-  //   }).pipe(
-  //     this.catchErrorPipe,
-  //     map(({response}) => response)
-  //   );
-  // }
 }
