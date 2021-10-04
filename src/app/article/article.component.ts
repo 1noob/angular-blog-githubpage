bd9651d25d0eacf25abd41103bf928e43415f1bc
1noob <448475691@qq.com>
2021-10-05 04:59:13 +0800

    fix bug

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute, ParamMap} from "@angular/router";
 import {switchMap} from "rxjs/operators";
 import {DomSanitizer, SafeResourceUrl, Title} from "@angular/platform-browser";
 import {NONE_TYPE} from "@angular/compiler";
-import {fromEvent, Observable} from 'rxjs';
+import {fromEvent, Observable, range} from 'rxjs';
 
 
 
@@ -52,13 +52,42 @@ export class ArticleComponent implements OnInit {
 
                 this.html = (new DOMParser).parseFromString(data.toString(), 'text/html');
 
+                // 移除自动生成的 href 跳转
+
+                let toc = this.html.getElementsByClassName('md-toc-inner');
+                for (let x of toc){
+                  x.removeAttribute('href');
+                }
+
+                // 修改下样式
+
+                let urls = this.html.getElementsByTagName('a');
+                for (let x of urls){
+                  this.renderer.setStyle(x,'color','#002FA7');
+                }
+
+                // 跳转监听
+
+                this.renderer.listen(document,'click',(e)=>{
+                  let el = e['originalTarget']['attributes']['class'];
+                  if (el && el['nodeValue']=='md-toc-inner'){
+                    el = e['originalTarget']['innerText'].toString().replaceAll(' ','-').toLowerCase();
+                    // console.log(el);
+                    this.myScroll(el);
+                  }
+                });
+
+                // 适配一下手机尺寸
+
                 if (screen.width<500){
-                  this.renderer.setStyle(this.html.getElementById('write'),'padding','0 0.5rem');
+                  this.renderer.setStyle(this.html.getElementById('write'),'padding','0 0.7rem');
                   this.renderer.setStyle(this.html.getElementById('write'),'font-size','0.7rem');
                 }
 
                 const html_el = this.html.documentElement as HTMLElement;
                 this.article = this.sanitizer.bypassSecurityTrustHtml(html_el.innerHTML);
+
+
 
               }
             }
@@ -66,22 +95,18 @@ export class ArticleComponent implements OnInit {
       }
     );
 
-    // fromEvent(window,'resize').subscribe(()=>{
-    //   // console.log(window.innerWidth);
-    //
-    //   if (!this.mobile && screen.width<500){
-    //     this.renderer.setStyle(this.html.getElementById('write'),'padding','0.7rem');
-    //     this.renderer.setStyle(this.html.getElementById('write'),'font-size','0.7rem');
-    //   }
-    //
-    //   const html_el = this.html.documentElement as HTMLElement;
-    //   this.article = this.sanitizer.bypassSecurityTrustHtml(html_el.innerHTML);
-    // });
-
   }
 
   ngAfterViewInit(){
 
+  }
+
+  /***
+   * TOC Anchor
+   * @Param id element id
+   */
+  myScroll(id: any){
+    document.getElementById(id).scrollIntoView({behavior: "smooth"});
   }
 
 
